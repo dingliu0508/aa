9d24258d161fcfb2ae666fec5255ae20e6c149ec
heJing <jinghedeveloper@126.com>
2019-06-22 12:15:15 +0800

    短信发送后端实现

--- meiduo_mall/meiduo_mall/apps/verifications/views.py
+++ meiduo_mall/meiduo_mall/apps/verifications/views.py
@@ -3,8 +3,11 @@ from django.views import View
 from django import http
 from meiduo_mall.libs.captcha.captcha import captcha
 from django_redis import get_redis_connection
-
+import random
 #1,生成图片验证码
+from meiduo_mall.libs.yuntongxun.sms import CCP
+
+
 class ImageCodeView(View):
     def get(self,request,image_code_id):
         #1,生成图片验证码
@@ -18,3 +21,44 @@ class ImageCodeView(View):
         #3,返回图片验证码
         return http.HttpResponse(image_data,content_type="image/png")
 
+#2,发送短信验证码
+class SmsCodeView(View):
+    def get(self,request,mobile):
+        #1,获取参数
+        image_code = request.GET.get("image_code")
+        image_code_id = request.GET.get("image_code_id")
+
+        #2,校验参数
+        #2,1 为空校验
+        if not all([image_code,image_code_id]):
+            return http.JsonResponse({"code":4001,"errmsg":"参数不完整"})
+
+        #2,2 获取redis中的图片验证码,校验为空
+        redis_conn = get_redis_connection("code")
+        redis_image_code = redis_conn.get("img_code_%s"%image_code_id)
+
+        #判断是否过期
+        if not redis_image_code:
+            return http.JsonResponse({"code": 4001, "errmsg": "图片验证码过期"})
+
+        #删除redis验证码
+        redis_conn.delete("img_code_%s"%image_code_id)
+
+        #2,3 图片验证码正确性
+        if image_code != redis_image_code.decode():
+            return http.JsonResponse({"code": 4001, "errmsg": "图片验证码错误"})
+
+        #3,发送短信,并判断是否发送成功
+        sms_code = "%06d"%random.randint(0,999999)
+        ccp = CCP()
+        result = ccp.send_template_sms(mobile, [sms_code, 5], 1)
+
+        #判断是否发送成功
+        if result == -1:
+            return http.JsonResponse({"code": 4001, "errmsg": "短信发送失败"})
+
+        #保存短信验证到redis
+        redis_conn.setex("sms_code_%s"%mobile,300,sms_code)
+
+        #4,返回响应
+        return http.JsonResponse({"code":'0',"errmsg":"ok"})
