3af40bf16bff2a3baaf9a3faa5ea3aa5f30b30cd
heJing <jinghedeveloper@126.com>
2019-06-27 09:50:17 +0800

    授权用户,数据校验

--- meiduo_mall/meiduo_mall/apps/oauth/views.py
+++ meiduo_mall/meiduo_mall/apps/oauth/views.py
@@ -4,8 +4,9 @@ from QQLoginTool.QQtool import OAuthQQ
 from django.conf import settings
 from django import http
 from .models import OAuthQQUser
-from django.contrib.auth import login
-from .utils import encode_openid
+from django.contrib.auth import login,authenticate
+from .utils import encode_openid,decode_openid
+from django_redis import get_redis_connection
 
 #1,获取qq登陆界面
 class QQLoginView(View):
@@ -70,3 +71,43 @@ class QQAuthUserView(View):
             response = redirect("/")
             response.set_cookie("username",user.username)
             return response
+
+    def post(self,request):
+        #1,获取参数
+        openid = request.POST.get("access_token")
+        mobile = request.POST.get("mobile")
+        password = request.POST.get("pwd")
+        sms_code = request.POST.get("sms_code")
+
+        #2,校验参数
+        #2,1 为空校验
+        if not all([openid,mobile,password,sms_code]):
+            return http.HttpResponseForbidden('参数不全')
+
+        #2,2 校验openid
+        openid = decode_openid(openid)
+
+        if not openid:
+            return http.HttpResponseForbidden("openid已过期")
+
+        #2,3 短信验证码校验
+        redis_conn = get_redis_connection("code")
+        redis_sms_code = redis_conn.get("sms_code_%s"%mobile)
+        if not redis_sms_code:
+            return http.HttpResponseForbidden("短信验证码已过期")
+
+        if sms_code != redis_sms_code.decode():
+            return http.HttpResponseForbidden("短信验证码错误")
+
+        #2,3 账号密码,正确性
+        user = authenticate(request, username=mobile, password=password)
+
+        #3,数据入库,判断用户对象
+        if user:
+            pass
+        else:
+            pass
+
+
+        #4,返回响应
+        pass
