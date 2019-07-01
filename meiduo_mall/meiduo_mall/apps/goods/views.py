66470e5e1adee20202b87d66e7fb664cea3c3c71
heJing <jinghedeveloper@126.com>
2019-07-01 11:06:23 +0800

    获取列表页商品

--- meiduo_mall/meiduo_mall/apps/goods/views.py
+++ meiduo_mall/meiduo_mall/apps/goods/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from django.views import View
+from meiduo_mall.utils.my_category import get_categories
+from .models import SKU
 
 #1,获取商品sku列表页面
 class SKUListView(View):
     def get(self,request,category_id,page_num):
-        return render(request,'list.html')
+
+        #1,获取分类信息
+        categories= get_categories()
+
+        #2,查询sku数据
+        skus = SKU.objects.filter(category_id=category_id).order_by("id")
+
+        #拼接数据返回响应
+        context = {
+            "categories":categories,
+            "skus":skus
+        }
+        return render(request,'list.html',context=context)
