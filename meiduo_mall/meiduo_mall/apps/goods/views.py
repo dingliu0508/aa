406d238a3091904849ddb87b857c62c072351c61
heJing <jinghedeveloper@126.com>
2019-07-01 11:49:00 +0800

    列表页分页

--- meiduo_mall/meiduo_mall/apps/goods/views.py
+++ meiduo_mall/meiduo_mall/apps/goods/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import View
 from meiduo_mall.utils.my_category import get_categories
 from .models import SKU,GoodsCategory
+from django.core.paginator import Paginator
 
 #1,获取商品sku列表页面
 class SKUListView(View):
@@ -13,13 +14,22 @@ class SKUListView(View):
         #2,查询sku数据
         skus = SKU.objects.filter(category_id=category_id).order_by("id")
 
+        #3,分页处理
+        paginate = Paginator(object_list=skus,per_page=5)
+        page = paginate.page(page_num)
+        current_page = page.number #当前页
+        skus_list = page.object_list #当前页对象列表
+        total_page = paginate.num_pages #总页数
+
         #3,获取分类对象
         category = GoodsCategory.objects.get(id=category_id)
 
         #拼接数据返回响应
         context = {
             "categories":categories,
-            "skus":skus,
-            "category":category
+            "skus":skus_list,
+            "category":category,
+            "current_page":current_page,
+            "total_page":total_page
         }
         return render(request,'list.html',context=context)
