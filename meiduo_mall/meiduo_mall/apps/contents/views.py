fc3c4c580d67b95eff22a733eeb71da81c0529f9
heJing <jinghedeveloper@126.com>
2019-07-01 09:16:59 +0800

    首页商品分类

--- meiduo_mall/meiduo_mall/apps/contents/views.py
+++ meiduo_mall/meiduo_mall/apps/contents/views.py
@@ -1,7 +1,43 @@
 from django.shortcuts import render
 from django.views import View
+from goods.models import GoodsChannel
 
 #1,获取首页
 class IndexView(View):
     def get(self,request):
-        return render(request,'index.html')
+
+        #1,定义字典
+        categories = {}
+
+        #2,获取频道(一级)
+        channels = GoodsChannel.objects.order_by("sequence")
+
+        #3,遍历频道
+        for channel in channels:
+            #3,1 获取组编号
+            group_id =  channel.group_id
+
+            #3,2组装数据
+            if group_id not in categories:
+                categories[group_id] = {"channels":[],"sub_cats":[]}
+
+            #3,3添加一级分类到字典中
+            category1 = channel.category
+            category1_dict = {
+                "id":category1.id,
+                "name":category1.name,
+                "url":channel.url
+            }
+            categories[group_id]["channels"].append(category1_dict)
+
+            #3,4获取二级分类,添加到字典中
+            sub_cats = category1.subs.all()
+            for cat2 in sub_cats:
+                categories[group_id]["sub_cats"].append(cat2)
+
+        #拼接数据,渲染页面
+        context = {
+            "categories":categories
+        }
+
+        return render(request,'index.html',context=context)
