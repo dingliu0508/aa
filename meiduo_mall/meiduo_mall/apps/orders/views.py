3c5685977251d6815e1553391dfcbb5754c04f0b
heJing <jinghedeveloper@126.com>
2019-07-07 09:13:23 +0800

    结算订单页展示地址,替换order_place.html文件

--- meiduo_mall/meiduo_mall/apps/orders/views.py
+++ meiduo_mall/meiduo_mall/apps/orders/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.views import View
+from users.models import Address
 
 #1,订单结算页
 class OrderSettlementView(View):
     def get(self,request):
-        return render(request,'place_order.html')
+
+        #1,查询用户地址
+        # Address.objects.filter(user_id=request.user.id).all()
+        addresses = request.user.addresses.filter(is_deleted=False).all()
+
+        #携带数据,渲染页面
+        context = {
+            "addresses":addresses
+        }
+        return render(request,'place_order.html',context=context)
