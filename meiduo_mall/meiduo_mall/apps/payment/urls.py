5913a5015d913b4cc64c195ea3cbce1b88a0ad24
heJing <jinghedeveloper@126.com>
2019-07-09 11:06:52 +0800

    支付结果保存类视图

--- meiduo_mall/meiduo_mall/apps/payment/urls.py
+++ meiduo_mall/meiduo_mall/apps/payment/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^payment/(?P<order_id>\d+)/$',views.AlipayView.as_view())
+    url(r'^payment/(?P<order_id>\d+)/$',views.AlipayView.as_view()),
+    url(r'^payment/status/$',views.PaymentStatusView.as_view()),
 ]
--- meiduo_mall/meiduo_mall/apps/payment/views.py
+++ meiduo_mall/meiduo_mall/apps/payment/views.py
@@ -5,6 +5,7 @@ from meiduo_mall.utils.response_code import RET
 from alipay import AliPay
 from orders.models import OrderInfo
 from django.conf import settings
+from .models import PaymentModel
 
 #1,获取支付页面
 class AlipayView(View):
@@ -43,3 +44,46 @@ class AlipayView(View):
         alipay_url = settings.ALIPAY_URL + '?' + order_string
 
         return http.JsonResponse({"code":RET.OK,"alipay_url":alipay_url})
+
+#2,支付状态保存
+class PaymentStatusView(View):
+    def get(self,request):
+        #1,获取参数
+        data = request.GET.dict()
+        signature = data.pop("sign")
+        out_trade_no = data.get("out_trade_no")
+        trade_no = data.get("trade_no")
+
+        #2,校验参数
+        #2,1 为空校验
+        if not all([signature,out_trade_no,trade_no]):
+            return http.HttpResponseForbidden("非法请求")
+
+        #2,2 校验整个data数据和sing
+        app_private_key_string = open(settings.ALIPAY_PRIVATE_PATH).read()
+        alipay_public_key_string = open(settings.ALIPAY_PUBLIC_PATH).read()
+        alipay = AliPay(
+            appid=settings.ALIPAY_APPID,
+            app_notify_url=None,  # 默认回调url
+            app_private_key_string=app_private_key_string,
+            # 支付宝的公钥，验证支付宝回传消息使用，不是你自己的公钥,
+            alipay_public_key_string=alipay_public_key_string,
+            sign_type="RSA2",  # RSA 或者 RSA2
+            debug = settings.ALIPAY_DEBUG  # 默认False
+        )
+        #取出data里面的数据重新加密, 和signature比对
+        success = alipay.verify(data, signature)
+
+        if not success:
+            return http.HttpResponseForbidden("非法请求,数据被篡改")
+
+        #3,数据入库
+        PaymentModel.objects.create(
+            out_trade_no_id=out_trade_no,
+            trade_no=trade_no
+        )
+
+        OrderInfo.objects.filter(order_id=out_trade_no).update(status=OrderInfo.ORDER_STATUS_ENUM["UNCOMMENT"])
+
+        #4,返回响应
+        return render(request,'pay_success.html',context={"order_id":out_trade_no})
